101d8662cc16ab715a5273a2f8e79981c2c41a56
Sanjiv. B <bas.sanjiiv@gmail.com>
2026-01-21 20:00:47 +0530

    Update View practice - 210126.sql

--- View practice - 210126.sql
+++ View practice - 210126.sql
@@ -1,24 +1,76 @@
-use emp_170122practice;
-select * from employee;
--- Creation of view
--- This used to create the view
-create view empl as
-select * 
-from employee
-where State = "Tamil Nadu";
+-- View
+-- ~~~~
+-- View is a virtual table  in MYSQL that will be created by using the queries. It does not not store data physically, only display the data from one or more tables.
 
--- Show view
-select * 
-from empl;
+create database empsal;
+use empsal;
 
--- update view
-create or replace view empl as
+create table emp
+(
+emp_id int,
+emp_name varchar(20),
+emp_dept varchar(20),
+emp_salary int
+);
+
+select * from emp;
+
+INSERT INTO emp VALUES
+(1, 'Sanjiv',   'IT',      45000),
+(2, 'Divya', 'HR',      30000),
+(3, 'Arun',   'Finance', 50000),
+(4, 'Karthik','IT',      55000),
+(5, 'Mani',   'Sales',   28000),
+(6, 'Vijay',  'HR',      32000),
+(7, 'Ajith',  'IT',      60000),
+(8, 'Prakash','Finance', 48000),
+(9, 'Ramesh', 'Sales',   35000),
+(10,'Sanjay', 'IT',      52000);
+
+-- 1. Creation of list of name starting with "S" 
+create view sys as
 select *
-from employee
-where state = "Uttar Pradesh" and Department = "Administration";
+from emp
+where emp_name like "s%";
+
+select * from sys;
+
+-- 2. Creation of list of employee between the ID 1 to 7 by using view
+create view emp1 as 
+select *
+from emp
+where emp_id between 1 and 7;
+
+select * from emp1;
+
+-- 3. Creation of list of view for the name Sanjiv & Divya by using view
+create view emp2 as 
+select *
+from emp
+where emp_name in ("Sanjiv","Divya");
+
+select * from emp2;
+
+-- 4. Creation of list of unique department by using view
+create view sys2 as
+select distinct(emp_dept)
+from emp
+group by emp_dept;
+
+select * from sys2;
+
+-- 5. List of employee names in asc by using view
+create view ascena as
+select *
+from emp
+order by emp_name asc;
+
+select * from ascena;
+
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- We could not use the three  things in view they are
 -- INSERT, UPDATE, DELETE. Examples are shown below
+-- there will be no output for the below shown examples
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 set sql_safe_updates = 0;
 create view emp as 
