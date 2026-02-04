a3b37bbad6083a10207d3a67f26a8a9f13b87883
Sanjiv. B <bas.sanjiiv@gmail.com>
2026-02-04 13:04:12 +0530

    Update Hospital_revenue_analysis.sql

--- Hospital_revenue_analysis.sql
+++ Hospital_revenue_analysis.sql
@@ -23,4 +23,50 @@ CREATE TABLE Doctor (
     'Dental Caries','Gingivitis','Periodontitis','Tooth Abscess','Impacted Tooth'),
 	Disease_Stage ENUM('Initial stage','Mid level','Final level'),
     foreign key (Pt_ID) references Patient(Pt_ID));
+
+CREATE TABLE Insurance (
+    Ins_ID VARCHAR(10) PRIMARY KEY,
+    DC_ID VARCHAR(10),
+    Ins_Name ENUM(
+        'Medicare',
+        'Tricare',
+        'Medicaid',
+        'BCBS',
+        'Cigna',
+        'Avmed',
+        'Alliance',
+        'Health Alliance'
+    ),
+    Ins_Status ENUM('Active','Inactive'),
+    Ins_From DATE,
+    Ins_To DATE,
+foreign key (DC_ID) references Doctor(DC_ID)
+);
+select * from insurance;
+CREATE TABLE Appointment (
+    App_ID VARCHAR(10) PRIMARY KEY,
+    Pt_ID VARCHAR(10),
+    DC_ID VARCHAR(10),
+    Ins_ID VARCHAR(10),
+    App_Status ENUM('Scheduled','Completed','Cancelled'),
+    App_TimePeriod DATETIME,
+	FOREIGN KEY (Pt_ID) REFERENCES Patient(Pt_ID),
+    FOREIGN KEY (DC_ID) REFERENCES Doctor(DC_ID),
+    FOREIGN KEY (Ins_ID) REFERENCES Insurance(Ins_ID)
+);
+
+CREATE TABLE Medicine (
+    MED_ID VARCHAR(10) PRIMARY KEY,
+    App_ID VARCHAR(10),
+    Pt_ID VARCHAR(10),
+    DC_ID VARCHAR(10),
+    Ins_ID VARCHAR(10), 
+    MED_Name VARCHAR(100),
+    MED_Dose VARCHAR(50),
+    FOREIGN KEY (Pt_ID) REFERENCES Patient(Pt_ID),
+    FOREIGN KEY (DC_ID) REFERENCES Doctor(DC_ID),
+    FOREIGN KEY (Ins_ID) REFERENCES Insurance(Ins_ID),
+    FOREIGN KEY (App_ID) References Appointment(app_id)
+   );
+
     
