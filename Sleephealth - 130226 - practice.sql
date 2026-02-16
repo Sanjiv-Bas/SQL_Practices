dc2c384203fa6be4d72e728ffea256a8fb400f06
Sanjiv. B <bas.sanjiiv@gmail.com>
2026-02-16 11:41:31 +0530

    Add SQL queries for health data analysis

--- Sleephealth - 130226 - practice.sql
+++ Sleephealth - 130226 - practice.sql
@@ -46,3 +46,98 @@ where Stress_level >7;
 select count(Gender) as  Male
 from sleephealth
 where Heart_Rate < 80 and Gender = "Male";
+
+-- 🔹 SUM() – 10
+-- 1 Total Daily Steps?
+select sum(daily_steps) as Total_steps
+from sleepdata;
+
+-- 2. Total Physical Activity Level?
+select sum(Physical_Activity_level)
+from sleephealth;
+
+-- 3  Total Stress Level of Females?
+select sum(Stress_Level) as Total_Stress
+from sleephealth
+where Gender = "Female";
+
+-- 4 Total Sleep Duration of Doctors?
+select sum(sleep_duration) as Total_Sleep
+from sleephealth
+where Occupation = "Doctor";
+
+-- 5 Total Heart Rate for Age > 50?
+select sum(Heart_Rate) as TotalHeartRate
+from sleephealth
+where Age > 50;
+
+-- 6 Total Steps of Obese category?
+select sum(Daily_Steps) as TotSteps
+from sleephealth;
+
+-- 7 Count people sleeping less than 6 hours?
+select count(*)
+from sleephealth
+where sleep_duration < 6;
+
+-- 8. Count distinct occupations?
+select distinct(occupation)
+from sleephealth;
+
+-- 9  Count people with Stress Level > 7?
+select count(*)
+from sleephealth
+where stress_level > 7;
+
+-- 10 Count males with heart rate above 80?
+select count(*)
+from sleephealth
+where Gender = "Male" and Heart_Rate > 80;
+
+-- 🔹 AVG() – 10
+-- 1 Average sleep duration?
+select avg(Sleep_Duration) as avgsleepduration
+from sleephealth;
+
+-- 2 Avg Stress Level?
+select avg(Stress_level) as AvgStress
+from sleephealth;
+
+-- 3 Avg Heart Rate of Females?
+select avg(Heart_Rate) as AvgHeartRate
+from sleephealth;
+
+-- 4. Avg Sleep Duration for Obese?
+select avg(sleep_duration) as avgsleep
+from sleephealth
+where BMI_Category = "Obese";
+
+-- 5 Avg Steps for Age > 40?
+select avg(steps) as avgsteps
+from sleephealth
+where Gender = 40; 
+
+-- 6 Avg Physical Activity for Doctors?
+select avg(Physical_Activity_Level) as AvgPhyLev
+from sleephealth
+where Occupation = "Doctor";
+
+-- 7 Avg Stress for people sleeping <6 hrs?
+select avg(Stress_Level) as avgsts
+from sleephealth 
+where sleep_duration < 6;
+
+-- 8 Avg heart rate of males?
+select avg(Heart_Rate)
+from sleephealth 
+where Gender = "male";
+
+-- 9 Avg sleep for people with disorder?
+select avg(sleep_duration)
+from sleephealth
+where sleep_disorder is not null;
+
+-- 10  Avg activity level for Normal BMI?
+select avg(Physical_Activity_Level) as avgphy
+from sleephealth
+where BMI_Category = "Normal";
